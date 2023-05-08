87f047fa7b7814df0ed2f6701e35c98e36ca3fd0
silvanapenkal <silvanapenkal@gmail.com>
2023-05-08 11:18:40 -0300

    implementa logs

--- src/main/java/tech/devinhouse/veiculos/controller/VeiculosController.java
+++ src/main/java/tech/devinhouse/veiculos/controller/VeiculosController.java
@@ -2,6 +2,7 @@ package tech.devinhouse.veiculos.controller;
 
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import tech.devinhouse.veiculos.model.Veiculo;
@@ -10,6 +11,7 @@ import tech.devinhouse.veiculos.service.VeiculoService;
 import java.net.URI;
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("api/veiculos")
 @AllArgsConstructor
@@ -19,33 +21,39 @@ public class VeiculosController {
 
     @GetMapping
     public ResponseEntity<List<Veiculo>> consultar () {
+        log.debug("Consulta da lista completa de veículos cadastrados");
         List<Veiculo> veiculos = service.consultar();
+        log.info("Existem {} veiculo(s) cadastrado(s)", veiculos.toArray().length);
         return ResponseEntity.ok(veiculos);
     }
 
 
     @GetMapping("{placa}")
     public ResponseEntity<List<Veiculo>> consultar (@PathVariable(value = "placa", required = false) String placa) {
-        List<Veiculo> veiculos = service.consultar(placa);
-        return ResponseEntity.ok(veiculos);
+        List<Veiculo> veiculo = service.consultar(placa);
+        log.debug("Veículo consultado {}", veiculo);
+        return ResponseEntity.ok(veiculo);
     }
 
     @PostMapping
-    public ResponseEntity<Veiculo> inserir (@RequestBody @Valid Veiculo request) {
-        Veiculo veiculo = request;
+    public ResponseEntity<Veiculo> inserir (@RequestBody @Valid Veiculo veiculo) {
+        log.debug("Dados do veículo a ser cadastrado: {} ", veiculo);
         veiculo = service.criar(veiculo);
+        log.info("Veiculo criado: {}", veiculo);
         return ResponseEntity.created(URI.create(veiculo.getPlaca())).body(veiculo);
     }
 
     @DeleteMapping("{placa}")
     public  ResponseEntity excluir(@PathVariable(value= "placa") String placa) {
         service.excluir(placa);
+        log.debug("veiculo excluido: {}", placa);
         return ResponseEntity.noContent().build();
     }
 
     @PutMapping("{placa}/multas")
     public ResponseEntity adicionarMulta(@PathVariable String placa){
         Veiculo veiculo = service.adicionarMulta(placa);
+        log.debug("Multa adiconada ao veículo: {}", veiculo);
         return ResponseEntity.ok(veiculo);
     }
 
--- src/main/java/tech/devinhouse/veiculos/service/VeiculoService.java
+++ src/main/java/tech/devinhouse/veiculos/service/VeiculoService.java
@@ -1,5 +1,6 @@
 package tech.devinhouse.veiculos.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import tech.devinhouse.veiculos.exception.RegistroExistenteException;
@@ -13,10 +14,12 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
+@Slf4j
 public class VeiculoService {
 
     @Autowired
     private VeiculoRepository veiculoRepository;
+
     public List<Veiculo> consultar(String placa) {
         Veiculo veiculo = veiculoRepository.findById(placa).orElseThrow(() -> new RegistroInexistenteException(placa));
         List <Veiculo> veiculos = new ArrayList<>();
@@ -30,8 +33,11 @@ public class VeiculoService {
     }
 
     public Veiculo criar(Veiculo veiculo) {
-        if (veiculoRepository.existsVeiculosByPlaca(veiculo.getPlaca()))
+        if (veiculoRepository.existsVeiculosByPlaca(veiculo.getPlaca())){
+            log.error("O veículo não foi cadastrado pois a placa {} já está cadastrada. ", veiculo.getPlaca());
             throw new RegistroExistenteException(veiculo.getPlaca());
+        }
+
         veiculo.setQtdMultas(0);
         veiculoRepository.save(veiculo);
         return veiculo;
@@ -39,12 +45,16 @@ public class VeiculoService {
 
     public void excluir(String placa) {
         boolean existe = veiculoRepository.existsVeiculosByPlaca(placa);
-        if (!existe)
+        if (!existe){
+            log.error("O veículo de placa {} não existe, por isso não foi excluído ", placa);
             throw new RegistroInexistenteException(placa);
+        }
         Optional<Veiculo> veiculoOptional = veiculoRepository.findById(placa);
         Integer multas = veiculoOptional.get().getQtdMultas();
-        if (multas != 0)
+        if (multas != 0){
+            log.error("O veículo de placa {} não foi excluído pois há {} multa(s) cadastrada(s). ", placa, multas);
             throw new VeiculoComMultaException(placa);
+        }
         veiculoRepository.deleteById(placa);
     }
 
